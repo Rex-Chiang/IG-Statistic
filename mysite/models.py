91999e0427b1c307c948d9089267eac92bf5ef1a
Rex-Chiang <m41045@gmail.com>
2019-04-11 19:37:40 +0800

    4/11

---
+++ mysite/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    account = models.CharField(max_length = 20)
+    pro = models.URLField(max_length = 300)
+    followers = models.IntegerField(default = 0)
+    followered = models.IntegerField(default = 0)
+    articles = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.account
+
+class PicInfo(models.Model):
+    account = models.CharField(max_length = 20)
+    pic = models.URLField(max_length = 300)
+    like = models.IntegerField(default = 0)
+    comment = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.account
